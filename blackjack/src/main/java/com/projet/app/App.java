bb1eba7161db97dd4112027662e06e4713f5a25b
Bogos-Printed <beauclair.nat@gmail.com>
2024-12-05 10:12:01 +0100

    fix: fix merge conflict

--- blackjack/src/main/java/com/projet/app/App.java
+++ blackjack/src/main/java/com/projet/app/App.java
@@ -1,11 +1,66 @@
 package com.projet.app;
 
+import java.util.List;
+import java.util.Scanner;
+
+import javax.smartcardio.Card;
+
+import com.projet.app.controller.Game;
 import com.projet.app.view.MainFrame;
 
 public class App {
   public static void main(String[] args)
   {
+    Scanner scanner = new Scanner(System.in);
+
+    Game game = new Game();
+
+    game.startGame();
+
+    System.out.println("Player's cards ");
+    hand(game.getPlayer().getHand());
+    System.out.println("Value of player's card : " + game.getPlayer().calculScore());
+    System.out.println("-------------------");
+    System.out.println("Dealer's cards");
+    hand(game.getDealer().getHand());
+    System.out.println("Value of dealer's card : " + game.getDealer().calculScore());
+
+    boolean playerTurn = true;
+
+    while (playerTurn) {
+      System.out.println("1: Hit, 2: Stand");
+      int choice = scanner.nextInt();
+
+      if (choice == 1) {
+          game.playerHit();
+          System.out.println("You hit : " + game.getPlayer().getHand().get(game.getPlayer().getHand().size() - 1));
+          System.out.println("Your hand :");
+          hand(game.getPlayer().getHand());
+          System.out.println("Player score " + game.getPlayer().calculScore());
+
+          if (game.getPlayer().isBust()) {
+              System.out.println("You have more than 21, you bust");
+              return;
+          }
+      
+      } else if (choice == 2){
+        playerTurn = false;
+      } else {
+          System.out.println("Error, choose 1 (Hit) or 2 (Stand).");
+      }
+    }
+
+    System.out.println("------------------------");
+    System.out.println(game.getWinner());
+    
     MainFrame app = new MainFrame();
     app.setVisible(true);
+  }
+
+  private static void hand(List<Card> hand) {
+    for (Card card : hand) {
+        System.out.println(card);
+  }
+
   }
 }
