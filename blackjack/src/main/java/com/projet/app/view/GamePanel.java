4b820f5cba320fed50801450ee407f7fa801df3f
Bogos-Printed <beauclair.nat@gmail.com>
2024-12-05 16:30:37 +0100

    feat(cards): add multiple cards to game panel

--- blackjack/src/main/java/com/projet/app/view/GamePanel.java
+++ blackjack/src/main/java/com/projet/app/view/GamePanel.java
@@ -7,17 +7,27 @@ import java.awt.*;
 public class GamePanel extends JPanel {
   
   public GamePanel(CardLayout layout, JPanel mainPanel) {
+    int col = 0;
+    int row = 0;
     ActionPanel actionPanel = new ActionPanel(layout, this, mainPanel);
-    ImageTest cardImage = new ImageTest(0, 0);
-    cardImage.setBackground(Color.BLACK);
-    
+    ImageTest cardImage = new ImageTest(col, row);
     setLayout(new BorderLayout());
 
-    JLabel label = new JLabel("Game Panel");
+    JPanel cards = new JPanel();
+    cards.setLayout(new BoxLayout(cards, BoxLayout.X_AXIS));
+    cards.setBackground(Color.RED);
 
-    add(cardImage, BorderLayout.CENTER);
-    add(cardImage);
-    add(label, BorderLayout.NORTH);
+    ImageTest cardImage2 = new ImageTest(1, row);
+    ImageTest cardImage3 = new ImageTest(2, row);
+    cardImage.setBackground(Color.ORANGE);
+
+    // JLabel label = new JLabel("Game Panel");
+
+    cards.add(cardImage);
+    cards.add(cardImage2);
+    cards.add(cardImage3);
+    add(cards, BorderLayout.CENTER);
+    // add(label, BorderLayout.NORTH);
     add(actionPanel, BorderLayout.SOUTH);
   }
 }
--- blackjack/src/main/java/com/projet/app/view/ImageTest.java
+++ blackjack/src/main/java/com/projet/app/view/ImageTest.java
@@ -3,8 +3,10 @@ package com.projet.app.view;
 import java.io.InputStream;
 
 import javax.imageio.ImageIO;
+import javax.swing.JComponent;
 import javax.swing.JPanel;
 
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
@@ -22,7 +24,10 @@ public class ImageTest extends JPanel {
 
       int largeurCarte = 62;
       int hauteurCarte = 92;
-      
+      Dimension dimension = new Dimension(largeurCarte, hauteurCarte);
+      setPreferredSize(dimension);
+      setMaximumSize(dimension);
+      setMinimumSize(dimension);
       cards = image.getSubimage(column * (largeurCarte + 7) + 18,
           row * (hauteurCarte + 6) + 17,
           largeurCarte, hauteurCarte);
@@ -36,7 +41,7 @@ public class ImageTest extends JPanel {
   protected void paintComponent(Graphics g) {
     super.paintComponent(g);
     if (cards != null) {
-      g.drawImage(cards, 62, 92, null);
+      g.drawImage(cards, 0, 0, null);
     }
   }
 }
