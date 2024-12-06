34bf0453ec0270f503b0880749a2ee9f9eabf9d9
Bogos-Printed <beauclair.nat@gmail.com>
2024-12-06 09:20:17 +0100

    feat(cards): add card generation via loop

--- blackjack/src/main/java/com/projet/app/view/GamePanel.java
+++ blackjack/src/main/java/com/projet/app/view/GamePanel.java
@@ -10,22 +10,26 @@ public class GamePanel extends JPanel {
     int col = 0;
     int row = 0;
     ActionPanel actionPanel = new ActionPanel(layout, this, mainPanel);
-    ImageTest cardImage = new ImageTest(col, row);
+    // ImageTest cardImage = new ImageTest(col, row);
     setLayout(new BorderLayout());
 
     JPanel cards = new JPanel();
     cards.setLayout(new BoxLayout(cards, BoxLayout.X_AXIS));
-    cards.setBackground(Color.RED);
+    // cards.setBackground(Color.RED);
 
     ImageTest cardImage2 = new ImageTest(1, row);
     ImageTest cardImage3 = new ImageTest(2, row);
-    cardImage.setBackground(Color.ORANGE);
 
     // JLabel label = new JLabel("Game Panel");
+    for (int i = 0; i <11; i++) {
+      ImageTest cardImage = new ImageTest(i, row);
 
-    cards.add(cardImage);
-    cards.add(cardImage2);
-    cards.add(cardImage3);
+      cards.add(cardImage);
+    }
+
+    // cards.add(cardImage);
+    // cards.add(cardImage2);
+    // cards.add(cardImage3);
     add(cards, BorderLayout.CENTER);
     // add(label, BorderLayout.NORTH);
     add(actionPanel, BorderLayout.SOUTH);
